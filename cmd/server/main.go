bc26c1f79695bd4d43a2d859a302bfbc3aa442e5
andrefelizardo <andreluis.felizardo@outlook.com>
2024-10-01 15:55:10 -0300

    Add currency exchange rate endpoint to server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
+	"time"
 )
+
+	
+type Cotacao struct {
+	Usdbrl struct {
+		Code       string `json:"code"`
+		Codein     string `json:"codein"`
+		Name       string `json:"name"`
+		High       string `json:"high"`
+		Low        string `json:"low"`
+		VarBid     string `json:"varBid"`
+		PctChange  string `json:"pctChange"`
+		Bid        string `json:"bid"`
+		Ask        string `json:"ask"`
+		Timestamp  string `json:"timestamp"`
+		CreateDate string `json:"create_date"`
+	} `json:"USDBRL"`
+}
 
 func main() {
 	http.HandleFunc("/cotacao", handlerCotacao)
@@ -18,7 +39,50 @@ func main() {
 }
 
 func handlerCotacao(w http.ResponseWriter, r *http.Request) {
+
+	
+	cotacao, err := getCotacaoDolar()
+	if err != nil {
+		http.Error(w, "Erro ao obter cotação", http.StatusInternalServerError)
+        return
+	}
+	
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "Cotação do dólar"}`))
+	w.Write([]byte(`{"message": "Cotação do dólar é ` + cotacao + `"}`))
+}
+
+func getCotacaoDolar() (string, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
+	if err != nil {
+		log.Printf("Erro ao criar requisição: %v", err)
+		return "", err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Printf("Erro ao fazer requisição: %v", err)
+		return "", err
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Erro ao ler corpo da resposta: %v", err)
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	var cotacao Cotacao
+	err = json.Unmarshal(body, &cotacao)
+	if err != nil {
+		log.Printf("Erro ao fazer parse do JSON: %v", err)
+		return "", err
+	}
+
+	
+	return cotacao.Usdbrl.Bid, nil
 }
